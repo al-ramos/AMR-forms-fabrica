e740d886a37ef267ec89d2ff7e27d07bd2d80b49
Alex ®amos <al-ramos@hotmail.com>
2026-05-11 19:39:05 -0300

    fix: reescreve index.css limpo com layout correto

--- amr-forms-fabrica-web/src/main.tsx
+++ amr-forms-fabrica-web/src/main.tsx
@@ -1,8 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
+import './App.css'
 import App from './App'
 
 const queryClient = new QueryClient()
@@ -14,3 +16,5 @@ createRoot(document.getElementById('root')!).render(
     </QueryClientProvider>
   </StrictMode>
 )
+
+
