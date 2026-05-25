a260b5426ddbdbd93a819e2605ad26e7e2cde8cf
Alex ®amos <al-ramos@hotmail.com>
2026-05-25 17:49:50 -0300

    feat: Setup Jest + React Testing Library configuration

---
+++ rds-forms-fabrica-web/jest.config.js
@@ -0,0 +1,17 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/main.tsx',
+    '!src/vite-env.d.ts',
+  ],
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+};
---
+++ rds-forms-fabrica-web/src/simple.test.ts
@@ -0,0 +1,9 @@
+describe('Jest Setup', () => {
+  it('should pass a simple test', () => {
+    expect(true).toBe(true);
+  });
+
+  it('should do basic math', () => {
+    expect(2 + 2).toBe(4);
+  });
+});
