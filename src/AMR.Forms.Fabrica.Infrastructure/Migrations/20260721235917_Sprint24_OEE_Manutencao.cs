87de74593b2ce08dda0e1280beea96453bde4a3e
Alessandro Ramos <alexsandro.ramos@gmail.com>
2026-07-21 21:01:42 -0300

    feat(fabrica): Sprint 24 – OEE e Manutenção Preventiva

---
+++ src/AMR.Forms.Fabrica.Infrastructure/Migrations/20260721235917_Sprint24_OEE_Manutencao.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AMR.Forms.Fabrica.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Sprint24_OEE_Manutencao : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
